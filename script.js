103d59aebbc8e6a5be4f8cbe2004bc3ed1b705f3
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-06-27 02:45:49 +0600

    game run timer done

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-let clutter ="";
+function makeBubble(){
+
+
+
+    let clutter ="";
 
 for( i=1;i<=160; i++){
 
@@ -7,3 +11,24 @@ for( i=1;i<=160; i++){
 }
 
 document.querySelector('#pbtm').innerHTML =clutter
+}
+
+var timer =1;
+function runTimer(){
+    let timerint =setInterval(function(){
+
+     if(timer>0){
+
+        timer--;
+     }
+     else{
+
+        clearInterval(timerint);// i did this bcz withouting clearing the time interval in background it will be run  randomly and it will take space... so for managing the memory soace i use this else option.. nothing else..
+     }
+        document.querySelector("#runtimeval").textContent=timer;
+    },1000);
+
+}
+runTimer();
+
+makeBubble();
