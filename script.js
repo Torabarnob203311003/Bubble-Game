6b8e77bcb5fc8c061fc70bffafc3335e151fc38c
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-06-29 00:22:13 +0600

    game done

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var timer =60;
 var score =0;
+var hitrn = 0;
  function increaseScore(){
 
   score += 10;
  document.querySelector('#scoreval').textContent = score;
 
   };
-
 function makeBubble(){
 
 
@@ -21,10 +21,6 @@ for( i=1;i<=160; i++){
 
 document.querySelector('#pbtm').innerHTML =clutter
 }
-
- 
-
-
 function runTimer(){
     let timerint =setInterval(function(){
 
@@ -35,22 +31,34 @@ function runTimer(){
      else{
 
         clearInterval(timerint);// i did this bcz withouting clearing the time interval in background it will be run  randomly and it will take space... so for managing the memory soace i use this else option.. nothing else..
+        document.querySelector("#pbtm").innerHTML =`<h1>Game Over</h1>
+        `
      }
         document.querySelector("#runtimeval").textContent=timer;
     },1000);
 
 }
-
-
 function getNewhit(){
 
-   let rn = Math.floor(Math.random()*10);
-   document.querySelector("#hitval").textContent= rn;
+    hitrn = Math.floor(Math.random()*10);
+   document.querySelector("#hitval").textContent= hitrn;
 
 };
 
 
 
+ document.querySelector('#pbtm').addEventListener("click",function(dets){
+
+var clickdum=( Number(dets.target.textContent));
+if(clickdum===hitrn){
+
+   increaseScore();
+   getNewhit();
+   makeBubble();
+ 
+}
+
+ });
 
 runTimer();
 
